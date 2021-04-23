aea276692f36eb4bea61f05eb240ee1a369a1181
nitishgarg2002 <nitishgarg2002@gmail.com>
2021-04-23 22:41:51 +0530

    courses

--- seeder.js
+++ seeder.js
@@ -24,7 +24,7 @@ const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`,'ut
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
-        await Course.create(courses);
+    //    await Course.create(courses);
         console.log('Data Imported..');
         process.exit();
     } catch (err) {
